0f23415388a93ab065fbdedad42baac504ea9dfc
tolache <anatoly.cherenkov@gmail.com>
2020-08-13 14:23:28 +0300

    update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,4 +1,4 @@
 param ([string] $param1 = "")
-Write-Output "param1: $param1"
-#$x = $param1.Replace("``n", "`n")
-#Write-Output "param1:$x"
+#Write-Output "param1: $param1"
+$x = $param1.Replace("``n", "`n")
+Write-Output "param1:$x"
