40db4a40e4c514c094f97fd8740a453e47197fff
Anatoly Cherenkov <32959649+tolache@users.noreply.github.com>
2021-03-03 11:58:46 +0300

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,11 +1,1 @@
-function Send-Keys
-{
-	notepad
-	$wshell = New-Object -ComObject wscript.shell;
-	$wshell.AppActivate('Notepad')
-	Sleep 1
-	$wshell.SendKeys('+2')
-	Sleep 1
-	$wshell.SendKeys('~')
-}
-Send-Keys
+echo Hi!
